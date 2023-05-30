1853112acf32fd62189cd252824527dfc454b02e
illyabezpaliy13 <110487623+illyabezpaliy13@users.noreply.github.com>
2023-05-30 19:42:34 +0300

    не доробив

---
+++ js/index1.js
@@ -0,0 +1,16 @@
+// . Доповни код таким чином,
+//  щоб у змінній result був результат виконання функції makePizza,
+//   а у змінній pointer було посилання на функцію makePizza.
+
+
+function makePizza() {
+
+  return "Your pizza is being prepared, please wait.";
+
+}
+    
+    // Change code below this line
+    
+    const result = makePizza();
+    
+    const pointer = makePizza;
---
+++ js/index2.js
@@ -0,0 +1,26 @@
+// Доповни функцію makeMessage таким чином, 
+// щоб вона очікувала другим параметром (параметр callback) колбек-функцію і повертала її виклик.
+//  Функція deliverPizza або makePizza буде передаватися як колбек і очікувати аргументом ім'я готової піци, що доставляється.
+
+
+
+function deliverPizza(pizzaName) {
+
+    return `Delivering ${pizzaName} pizza.`;
+
+  }
+
+  function makePizza(pizzaName) {
+
+    return `Pizza ${pizzaName} is being prepared, please wait...`;
+
+  }
+
+  // Chande code below this line
+
+  function makeMessage(pizzaName) {
+
+    return;
+
+  }
+
